61576b2d040a1e6d5119b7f4743270aece22a18a
rasyidch <rasyidchamiedy@gmail.com>
2020-04-15 20:38:51 +0800

    toUpperCase untuk ... toLowerCase untuk ... substring(5) untuk ... substring(0,4) untuk ...

--- src/String/OperasiString.java
+++ src/String/OperasiString.java
@@ -9,5 +9,11 @@ public class OperasiString {
         char[] Warwickchar = {'W','A','R','W','I','C','K'};
         String WarwickString = new String(Warwickchar);
         System.out.println(WarwickString);
+
+        System.out.println(City.toUpperCase());
+        System.out.println(City.toLowerCase());
+        System.out.println(City.substring(5));
+        System.out.println(City.substring(0,4));
+
     }
 }
