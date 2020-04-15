3cee7aa95ec6f9ac20cd3ed9fce2bf1c8e5b7f97
rasyidch <rasyidchamiedy@gmail.com>
2020-04-15 21:18:36 +0800

    Operasi Binary Assignment

---
+++ src/OperatorBinary.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Ibadur Rasyid Chamiedy";
+    int JumlahSks = "24";
+    double IndeksPrestasiKumulatif = 3.90;
+
+}
